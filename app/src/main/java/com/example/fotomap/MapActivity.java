41a9078c0697cca520c0c8aae35304b7b4e424af
timho <timsen98@web.de>
2020-06-28 14:02:20 +0200

    Map activity updated

--- app/src/main/java/com/example/fotomap/MapActivity.java
+++ app/src/main/java/com/example/fotomap/MapActivity.java
@@ -1,6 +1,29 @@
 package com.example.fotomap;
 
-import android.app.Activity;
+import android.os.Bundle;
 
-public class MapActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.OnCreate(SavedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
 }
