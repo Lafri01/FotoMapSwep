13b1336033520187dd9011c88f3ace8ffcaacb90
Lafri01 <67288609+Lafri01@users.noreply.github.com>
2020-07-08 11:36:18 +0200

    Delete Constants.java

--- app/src/main/java/com/example/fotomap/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.fotomap;
-
-public class Constants {
-
-    public static final String UPLOAD_URL = "http://192.168.94.1/AndroidImageUpload/upload.php";
-    public static final String IMAGES_URL = "http://192.168.94.1/AndroidImageUpload/getImages.php";
-}
