7e46d0ebbdd9c6df7ed8ff20d5611f324a2d7beb
TimRiddle23 <67287762+TimRiddle23@users.noreply.github.com>
2020-07-08 12:17:23 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/fotomap/Constants.java
@@ -0,0 +1,10 @@
+package com.example.fotomap;
+
+public class Constants {
+
+    public static final String UPLOAD_URL = "http://192.168.94.1/AndroidImageUpload/upload.php";
+    public static final String IMAGES_URL = "http://192.168.94.1/AndroidImageUpload/getImages.php";
+
+    public static final int PERMISSION_REQUEST_ENABLE_GPS = 9002;
+    public static final int PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 9003;
+}
